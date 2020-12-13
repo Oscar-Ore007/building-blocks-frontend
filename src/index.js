603866578189a14cd454ae92438e23c81184aacc
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-12-12 21:30:15 -0500

    added js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("building blocks")
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
+    const width = 10 
+    let nextRandom = 0 
+    let timerId
+    let score = 0 
+    const colors = [
+        'orange',
+        'red',
+        'purple',
+        'green',
+        'blue'
+    ]
+
+
+})
