c645ab79e212d897027d03e927f53368e83f7f7b
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-12-13 13:54:17 -0500

    added ability to move buildingblocks right and left

--- src/index.js
+++ src/index.js
@@ -79,16 +79,63 @@ document.addEventListener('DOMContentLoaded', () => {
 //moving the building blocks down 
 timerId = setInterval(moveDown, 1000)
 
+//assigning functions to keyCodes 
+function control(e) {
+    if(e.keyCode === 37) {
+        moveleft()
+    } else if (e.keyCode === 38) {
+        rotate()
+    } else if (e.keyCode === 39) {
+        moveRight()
+    } else if (e.keyCode === 40) {
+        moveDown()
+    }
+}
+document.addEventListener('keyup', control)
 
 // move down function 
 function moveDown() {
     undraw()
     currentPosition += width
     draw()
+    freeze()
 }
 
-//freeze fucntion to stop the building blocks on page
+//freeze function to stop the building blocks on page
+function freeze() {
+   if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        //starting anew building block fall
+        random = Math.floor(Math.random() * theBuildingblocks.length)
+        current = theBuildingblocks[random][currentRotation]
+        currentPosition = 4 
+        draw()
+   }
+}
 
+//move the building blocks left 
+function moveleft() {
+    undraw() 
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width ===0)
+
+    if(!isAtLeftEdge) currentPosition -=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition +=1
+    }
+    draw()
+}
+
+//move the buildingblocks right unless the its on the edge or space is taken
+function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+    if(!isAtRightEdge) currentPosition +=1
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -=1 
+    }
+    draw()
+}
 
 
 
